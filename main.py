ee6a0c1d952c570913d95cb8dd97c7a27cd17664
iam-salma <salmasyed1360@gmail.com>
2025-06-25 20:47:17 +0530

    render compatibility update

--- main.py
+++ main.py
@@ -320,4 +320,4 @@ async def startup_event():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="00.0.0, port=os.environ["PORT"])
